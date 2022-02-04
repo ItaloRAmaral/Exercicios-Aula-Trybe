fa5503fee6ad26007d54658546a7aa9ebb5858fa
Italo Amaral <italorockamaral@gmail.com>
2022-02-04 12:42:47 -0300

    exercicios do dia

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02/exercicio5.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeromaior = numbers[0];
+
+for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index]> numeromaior) {
+        numeromaior = numbers[index];
+    }
+}
+
+
+
+
+
+
+console.log(numeromaior);
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02/exercicio7.js
@@ -0,0 +1,21 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeromenor = numbers[0];
+
+for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index]< numeromenor) {
+        numeromenor = numbers[index];
+    }
+}
+
+
+
+
+
+
+console.log(numeromenor);
+
+
+
+
+
+
