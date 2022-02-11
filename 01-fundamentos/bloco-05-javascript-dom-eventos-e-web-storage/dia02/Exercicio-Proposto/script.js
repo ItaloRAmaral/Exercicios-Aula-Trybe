573dc17769b6cd4140917ad825f182616733b52d
Italo Amaral <italorockamaral@gmail.com>
2022-02-10 21:33:44 -0300

    tudo finalizado

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia02/Exercicio-Proposto/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia02/Exercicio-Proposto/script.js
@@ -22,7 +22,7 @@ elementMain.appendChild(section);
 /// 4 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
 
 let paragraph = document.createElement('p');
-paragraph.innerText = "EU TO ERRANDO TUDOOOOO DE ESCRITAAAAA!!!!!"
+paragraph.innerText = "TESTE TESTE TESTE TESTE TESTE TESTE!"
 section.appendChild(paragraph);
 
 /// 5 - Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
@@ -63,6 +63,41 @@ for (let i = 0; i < 3; i += 1) {
     tagsh3.innerHTML = "Aplicando h3" +' ' + i;
     elementMain.appendChild(tagsh3);
 }
+
+
+/// Agora que você criou muita coisa, vamos fazer algumas alterações e remoções:
+/// 1 - Adicione a classe title na tag h1 criada;
+
+text.className = "title"; 
+
+/// 2 - Adicione a classe description nas 3 tags h3 criadas;
+
+const tagsh3 = document.getElementsByTagName('h3');
+
+for (let i = 0; i < 3; i += 1) {
+    tagsh3[i].className = "description";   
+}
+
+/// 3 - Remova a section criado no passo 5(aquele que possui a classe left - content).Utilize a função.removeChild();
+
+// let removeLeftContent = elementMain.firstElementChild.nextElementSibling
+
+// elementMain.removeChild(removeLeftContent);
+
+
+/// 4 - Centralize a section criado no passo 6(aquele que possui a classe right - content).Dica: para centralizar, basta configurar o margin - right: auto da section;
+
+sectionTres.style.marginRight = 'auto';
+
+/// 5 - Troque a cor de fundo do elemento pai da section criada no passo 3(aquela que possui a classe center - content) para a cor verde;
+
+section.style.backgroundColor = "green";
+
+/// 6 - Remova os dois últimos elementos(nove e dez) da lista criada no passo 8.
+
+lista.lastChild.remove();
+lista.lastChild.remove();
+
 
 
 
