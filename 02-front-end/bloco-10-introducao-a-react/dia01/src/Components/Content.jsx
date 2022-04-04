007d6d05c2764cdad81023e62b86677e7ddf0d9b
Italo Amaral <italorockamaral@gmail.com>
2022-04-04 18:30:46 -0300

    Primeiro Dia de React

---
+++ 02-front-end/bloco-10-introducao-a-react/dia01/src/Components/Content.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const conteudos = [
+    {
+        conteudo: 'High Order Functions',
+        bloco: 8,
+        status: 'Aprendido'
+    },
+    {
+        conteudo: 'Composicao de Componentes',
+        bloco: 11,
+        status: 'Aprendendo',
+    },
+    {
+        conteudo: 'Composicao de Estados',
+        bloco: 12,
+        status: 'Aprenderei'
+    },
+    {
+        conteudo: 'Redux',
+        bloco: 16,
+        status: 'Aprenderei'
+    },
+]
+
+class Content extends React.Component {
+    render() {
+        return(
+            <section className="content">
+                {conteudos.map((element)=>(
+                    <div key={element.conteudo}>
+                        <h4>{`O conteudo é ${element.conteudo}`}</h4>
+                        <p>{`Bloco: ${element.bloco}`}</p>
+                        <p>{`Status" ${element.status}`}</p>
+                    </div>
+                ))}
+            </section>
+        )
+    }
+}
+
+export default Content;
