43dd3a7ecf7a8378cb6f60bd35468b6e307477a1
Italo Amaral <italorockamaral@gmail.com>
2022-04-07 14:01:05 -0300

    Exercicio de evento react

---
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/componentes-com-estados-e-eventos/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Clique from './components/Clique'
+
+function App() {
+  return (
+    <section>
+      <p>Teste</p>
+      <Clique />
+    </section>
+  );
+}
+
+export default App;
