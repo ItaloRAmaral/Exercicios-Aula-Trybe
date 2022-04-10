ea3f91942b788d3aeb5d9017a659e5b4ef036074
Italo Amaral <italorockamaral@gmail.com>
2022-04-09 23:46:41 -0300

    Exercicio de Formulario Que tiRou meu sono ok

---
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/exercicio-de-fixacao/src/App.js
@@ -0,0 +1,12 @@
+import Forms from './Components/Forms'
+import './App.css';
+
+function App() {
+  return (
+    <section className="App">
+      <Forms />
+    </section>
+  );
+}
+
+export default App;
